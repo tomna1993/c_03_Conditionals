a5d94929e113ce325142b228ff6dc45b8d451168
TomasNagy <nagy.tamas.guitar@gmail.com>
2023-04-25 17:04:03 +0200

    Program complete

--- conditionals.c
+++ conditionals.c
@@ -2,5 +2,22 @@
 
 int main(void)
 {
+	int x, y;
+
+	printf("Give the first number: ");
+	scanf("%d\n", &x);
+
+	printf("Give the second number: ");
+	scanf("%d\n", &y);
 	
+	if (x < y)
+	{
+		printf("%d is less than %d", x, y);
+	}
+	else if (x > y)
+	{
+		printf("%d is greater than %d", x, y);
+	}
+	else
+		printf("The two numbers are equal!");
 }
